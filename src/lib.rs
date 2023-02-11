76727e73f413063ade5f5bfbebbdb8f408519c6b
ByteDream <bytedream@protonmail.com>
2023-02-11 19:17:56 +0100

    docs: add documentation to macro function

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,28 @@ use syn::{parse_macro_input, Item};
 
 mod expand;
 
+/// The main macro of this crate.
+/// Use it to define default values of fields in structs you [`Serialize`] or [`Deserialize`].
+/// You do not need to create a extra function to provide the default value, as it is the case in serdes' implementation of default (`#[serde(default = "...")]`).
+///
+/// Set this macro on a struct where you use [`Serialize`] or [`Deserialize`] and use `#[serde_inline_default(...)]` on the field you want to have a inline default value.
+/// Replace the `...` with the value you want and it will be set as default if serde needs it.
+///
+/// Note that you must set this macro _before_ `#[derive(Serialize)]` / `#[derive(Deserialize)]` as it wouldn't work properly if set after the derive.
+///
+/// # Examples
+///
+/// ```rust
+/// #[serde_inline_default]
+/// #[derive(Deserialize)]
+/// struct Test {
+///     #[serde_inline_default(42)]
+///     value: u32
+/// }
+/// ```
+///
+/// [`Serialize`]: https://docs.rs/serde/*/serde/trait.Serialize.html
+/// [`Deserialize`]: https://docs.rs/serde/*/serde/trait.Deserialize.html
 #[proc_macro_attribute]
 pub fn serde_inline_default(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as Item);
