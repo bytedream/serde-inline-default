301f623073175c4e042669daaa019d17d4287e42
kaphula <vaaka@protonmail.com>
2025-01-15 16:11:16 +0200

    Change macro name, comment example. Fmt.

--- examples/parent_defaults.rs
+++ examples/parent_defaults.rs
@@ -1,27 +1,28 @@
 use serde::{Deserialize, Serialize};
-use serde_json::json;
 use serde_inline_default::serde_inline_default;
+use serde_json::json;
 
 #[serde_inline_default]
 #[derive(Serialize, Deserialize)]
 struct A {
-   #[serde_inline_parent()]
-   value: String,
-}
-impl Default for A {
-   fn default() -> Self {
-      Self {
-         value: "my cool default".to_string(),
-      }
-   }
+    /// Default value will be set from [A]'s default implementation,
+    /// like this: `A::default().value`
+    #[serde_inline_default_parent()]
+    value: String,
 }
 
+const DEFAULT_STRING_VALUE: &str = "my cool default";
+impl Default for A {
+    fn default() -> Self {
+        Self {
+            value: DEFAULT_STRING_VALUE.into(),
+        }
+    }
+}
 
 fn main() -> Result<(), serde_json::Error> {
-   let json_object = json!({});
-   let basic: A = serde_json::from_value(json_object)?;
-   assert_eq!(basic.value, "my cool default".to_string());
-
-
-   Ok(())
+    let json_object = json!({});
+    let basic: A = serde_json::from_value(json_object)?;
+    assert_eq!(basic.value, DEFAULT_STRING_VALUE);
+    Ok(())
 }
--- src/expand.rs
+++ src/expand.rs
@@ -3,70 +3,64 @@ use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
 use syn::{parse_quote, ItemStruct};
 
-
-
-const SERDE_INLINE_PARENT: &str = "serde_inline_parent";
+const SERDE_INLINE_PARENT: &str = "serde_inline_default_parent";
 const SERDE_INLINE_DEFAULT: &str = "serde_inline_default";
 pub(crate) fn expand_struct(mut item: ItemStruct) -> proc_macro::TokenStream {
     let mut inline_fns: Vec<TokenStream> = vec![];
 
     for (i, field) in item.fields.iter_mut().enumerate() {
         for (j, attr) in field.attrs.iter_mut().enumerate() {
+            if attr.path().is_ident(SERDE_INLINE_DEFAULT) {
+                let default: TokenStream = attr.parse_args().unwrap();
 
-           if attr.path().is_ident(SERDE_INLINE_DEFAULT) {
-              let default: TokenStream = attr.parse_args().unwrap();
+                let fn_name_lit = format!("__{}_{}_{}", SERDE_INLINE_DEFAULT, item.ident, i);
+                let fn_name_ident = Ident::new(&fn_name_lit, Span::call_site());
+                let mut return_type = field.ty.clone();
 
-              let fn_name_lit = format!("__{}_{}_{}", SERDE_INLINE_DEFAULT, item.ident, i);
-              let fn_name_ident = Ident::new(&fn_name_lit, Span::call_site());
-              let mut return_type = field.ty.clone();
+                // replaces most lifetimes with 'static
+                type_lifetimes_to_static(&mut return_type);
 
-              // replaces most lifetimes with 'static
-              type_lifetimes_to_static(&mut return_type);
+                inline_fns.push(quote! {
+                    #[doc(hidden)]
+                    #[allow(non_snake_case)]
+                    fn #fn_name_ident () -> #return_type {
+                        #default
+                    }
+                });
 
-              inline_fns.push(quote! {
-                #[doc(hidden)]
-                #[allow(non_snake_case)]
-                fn #fn_name_ident () -> #return_type {
-                    #default
-                }
-            });
+                field.attrs.remove(j);
+                field
+                    .attrs
+                    .insert(j, parse_quote!( #[serde(default = #fn_name_lit)] ));
+                break;
+            } else if attr.path().is_ident(SERDE_INLINE_PARENT) {
+                let fn_name_lit = format!("__{}_{}_{}", SERDE_INLINE_PARENT, item.ident, i);
+                let fn_name_ident = Ident::new(&fn_name_lit, Span::call_site());
+                let mut return_type = field.ty.clone();
 
-              field.attrs.remove(j);
-              field
-                 .attrs
-                 .insert(j, parse_quote!( #[serde(default = #fn_name_lit)] ));
-              break;
+                // replaces most lifetimes with 'static
+                type_lifetimes_to_static(&mut return_type);
 
-           }
-           else if attr.path().is_ident(SERDE_INLINE_PARENT) {
+                let parent_default_field_name = field.clone().ident.expect("missing parent field");
+                let default: TokenStream =
+                    format!("{}::default().{}", item.ident, parent_default_field_name)
+                        .parse()
+                        .expect("failed to parse TokenStream");
 
-              let default: TokenStream = attr.parse_args().unwrap();
+                inline_fns.push(quote! {
+                    #[doc(hidden)]
+                    #[allow(non_snake_case)]
+                    fn #fn_name_ident () -> #return_type {
+                        #default
+                    }
+                });
 
-              let fn_name_lit = format!("__{}_{}_{}", SERDE_INLINE_PARENT, item.ident, i);
-              let fn_name_ident = Ident::new(&fn_name_lit, Span::call_site());
-              let mut return_type = field.ty.clone();
-
-              // replaces most lifetimes with 'static
-              type_lifetimes_to_static(&mut return_type);
-
-              let parent_default_field_name = field.clone().ident.expect("missing field");
-              let default_grab: TokenStream = format!("{}::default().{}", item.ident, parent_default_field_name).parse().expect("fail");
-
-              inline_fns.push(quote! {
-                #[doc(hidden)]
-                #[allow(non_snake_case)]
-                fn #fn_name_ident () -> #return_type {
-                    #default_grab
-                }
-            });
-
-              field.attrs.remove(j);
-              field
-                 .attrs
-                 .insert(j, parse_quote!( #[serde(default = #fn_name_lit)] ));
-              break;
-
-           }
+                field.attrs.remove(j);
+                field
+                    .attrs
+                    .insert(j, parse_quote!( #[serde(default = #fn_name_lit)] ));
+                break;
+            }
         }
     }
 
